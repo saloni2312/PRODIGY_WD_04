7039b4022bae74fdc8ce03adbd3d52eebf945652
Saloni <152170121+saloni2312@users.noreply.github.com>
2024-09-17 21:33:32 +0530

    Add files via upload

---
+++ Landing page/script.js
@@ -0,0 +1,5 @@
+// Here you can handle the login functionality, such as opening a login form modal or redirecting to a login page.
+
+document.querySelector(".login-btn").addEventListener("click", function() {
+    alert("Login functionality goes here!");
+});
